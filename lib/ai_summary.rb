460cdf516597e8c0f9df3226cc1e851634c9885a
andersmarkc <andersmarkc@gmail.com>
2025-07-23 16:35:09 +0200

    Base Version

---
+++ lib/ai_summary.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ai_summary/version"
+
+module AiSummary
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ai_summary/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module AiSummary
+  VERSION = "0.1.0"
+end
